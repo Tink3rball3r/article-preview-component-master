79fd23d9dca3a3d2603f5645f3763e75e7f49388
Tink3rball3r <matthew_rittenhouse@hotmail.com>
2021-07-10 20:57:30 +0900

    js and share menu complete for mobile

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('You can do this!');
+const shareBtn = document.getElementById('share-btn');
+const socialLinks = document.getElementById('social-links');
+const shareArrow = document.querySelector('svg path');
+
+shareBtn.addEventListener('click', () => {
+	shareArrow.classList.toggle('open');
+	shareBtn.classList.toggle('open');
+	socialLinks.classList.toggle('hidden');
+});
